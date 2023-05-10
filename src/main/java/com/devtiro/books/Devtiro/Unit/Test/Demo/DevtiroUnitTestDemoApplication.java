b140c6d19ab5d682978db5a8debf3e8c2785229d
Aaron Bourne <aarondbourne@gmail.com>
2023-05-10 16:43:11 +0100

    Fresh download from Spring Boot Initialzr

---
+++ src/main/java/com/devtiro/books/Devtiro/Unit/Test/Demo/DevtiroUnitTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devtiro.books.Devtiro.Unit.Test.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtiroUnitTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtiroUnitTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devtiro/books/Devtiro/Unit/Test/Demo/DevtiroUnitTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtiro.books.Devtiro.Unit.Test.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtiroUnitTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
